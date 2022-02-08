9d65668828af7a06187c1a4398e7578ec25b510f
SATO Yusuke <yusuke.sato.zz@gmail.com>
2022-02-09 01:05:28 +0900

    Add license text to source files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # JISX0213String
+
 Library to handle JIS X 0213 Characters in strings
+
+- This library (JISX0213String.java) is relatively small, because it uses
+  x-SJIS_0213 Encoding object contained in JDK and does not contain
+  the whole definition of JIS X 0213 character set.
+- This library handles non-kanji characters in JIS X 0213 collectly.
+  The existing implementation of JIS X 0213 validator (e.g. JIS_X_0213_Kanji.java
+  in TERASOLUNA) does not contain definitions of non-kanji characters in JIS X 0213.
--- src/test/com/example/JISX0213StringTest.java
+++ src/test/com/example/JISX0213StringTest.java
@@ -1,3 +1,18 @@
+/*
+Copyright 2022 SATO Yusuke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.example;
 
 import static org.junit.Assert.*;
@@ -60,6 +75,8 @@ public class JISX0213StringTest {
         assertFalse(JISX0213String.isValid("¢"));
         assertFalse(JISX0213String.isValid("£"));
         assertFalse(JISX0213String.isValid("\u00ac")); // ¬（半角の￢）
+
+        // TODO: Σはinvalidでよいか？
 
         // JIS X 0213 JIS第3水準非漢字 合字の2コードユニット目だけ
         assertFalse(JISX0213String.isValid("\u309a"));
